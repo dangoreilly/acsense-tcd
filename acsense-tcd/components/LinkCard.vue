dc4c038274aa174c53e9072542a3ceff8c2b4330
Dan O'Reilly <dangoreilly@gmail.com>
2023-05-08 11:40:04 +0100

    Added placeholder card template

--- acsense-tcd/components/LinkCard.vue
+++ acsense-tcd/components/LinkCard.vue
@@ -1,9 +1,16 @@
 <template>
+<div class="card mb-3">
 
-    <div class="card">
-        
+    <div class="card-header">
+        <h6 class="m-0">
+            <strong><slot></slot></strong>
+        </h6>
+    </div>
+    <div class="card-body">
+        <em> Coming soon </em>
     </div>
 
+</div>
 </template>
 
 <script lang="ts">
