4ed1c13c6bf329af20e3add1e01416ad8739cfe4
Dan O'Reilly <dangoreilly@gmail.com>
2023-05-13 21:31:06 +0100

    Initialised building management page

---
+++ acsense-tcd/pages/admin/buildingData.vue
@@ -0,0 +1,34 @@
+<template>
+    <main class="d-flex flex-nowrap" style="height:100vh">
+        <AdminSidebar/>
+        <div class="container border border-3 border-danger w-auto"> 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi nobis sit omnis doloribus aliquid eum suscipit in tempore? Dicta nam nemo voluptate veritatis repellendus vitae optio tempora minus cupiditate dolorem?
+        </div>
+    </main>
+</template>
+
+<script>
+
+export default {
+    data(){
+        
+    }
+}
+</script>
+
+<style>
+html {
+  height: -webkit-fill-available;
+}
+body {
+    min-height: 100vh;
+    min-height: -webkit-fill-available;
+}
+main {
+  min-height: 100vh;
+  min-height: -webkit-fill-available;
+  max-height: 100vh;
+  overflow-x: auto;
+  overflow-y: hidden;
+}
+</style>
