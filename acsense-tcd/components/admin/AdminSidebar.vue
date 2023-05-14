8b5b6bf89cf25c340f239b3045badd4ccff1cb0b
Dan O'Reilly <dangoreilly@gmail.com>
2023-05-14 01:03:10 +0100

    Fixed bootstrap-icons support

--- acsense-tcd/components/admin/AdminSidebar.vue
+++ acsense-tcd/components/admin/AdminSidebar.vue
@@ -77,12 +77,12 @@ useHead({
             content: 'Acsense Admin page'
         }
     ],
-    link: [
-        {
-            rel: 'stylesheet',
-            href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css'
-        }
-    ]
+    // link: [
+    //     {
+    //         rel: 'stylesheet',
+    //         href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css'
+    //     }
+    // ]
 })
 
 </script>
