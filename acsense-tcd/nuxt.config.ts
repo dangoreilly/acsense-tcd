82af32d590032644868b514b6f315c0caa19ee00
Dan O'Reilly <dangoreilly@gmail.com>
2023-05-14 19:12:01 +0100

    Moved infopage components into info folder

--- acsense-tcd/nuxt.config.ts
+++ acsense-tcd/nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     routeRules: {
-        // '/**': {static: true},
+        '/**': {static: true},
     },
     css: [ "~/assets/css/style.scss"],
     modules: [
         '@vueuse/nuxt',
       ],
-
+    components: [
+        { path: '~/components/admin'},
+        { path: '~/components/info'},
+        '~/components'
+    ]
 })
