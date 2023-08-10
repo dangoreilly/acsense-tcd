281ad4b3fbcc6d0552364230f7c11583fb9fa7dd
dangoreilly <92057656+dangoreilly@users.noreply.github.com>
2023-08-10 19:21:05 +0000

    Added route for getting all basic building data (for map render)

---
+++ acsense-tcd/server/api/get/building/all.js
@@ -0,0 +1,47 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+// The all buildings endpoint will only return the primary building data
+// It will not return any student spaces or gallery images associated with the building
+
+export default defineEventHandler(async (event) => {
+
+    // console.table(event)
+    let buildingId = event.context.params.buildingId;
+
+    // Get the primary building data
+    let building_response_data = await getBuildingData(buildingId);
+
+    return building;
+
+  })
+
+async function getBuildingData(buildingId) {
+    let { data: building, error } = await supabase
+        .from('buildings')
+        .select('*')
+        .eq('canonical', buildingId)
+    if (error) {
+        console.log(error)
+        throw error
+    }
+    // console.log("Building found: " + building[0].display_name + " (" + building[0].canonical + ")");
+    return building;
+}
+
+async function getSpacesForBuilding(buildingId) {
+    let { data: spaces, error } = await supabase
+        .from('spaces')
+        .select('*')
+        .eq('building', buildingId)
+    if (error) {
+        console.log(error)
+        throw error
+    }
+    console.log("Spaces found:");
+    console.log(spaces);
+    return spaces;
+}
