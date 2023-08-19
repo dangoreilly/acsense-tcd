2247837cace6f4ecf4c1fa6ce4b05b245e84bf78
Dan O'Reilly <dangoreilly@gmail.com>
2023-08-19 10:24:17 +0100

    init

---
+++ acsense-tcd/components/admin/AdminBuildingSelector.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="d-flex flex-column flex-shrink-0 py-3 px-3 border-end border-secondary-subtle bg-body-tertiary position-sticky" style="width: 200px; overflow-y: auto;">
+        <ul class="nav nav-pills flex-column mb-auto">
+            <!-- Highlight the active page by matching it to the activeBuilding -->
+            <li v-for="building in buildings">
+                <div
+                @click="$emit('update:activeBuilding', building)"
+                class="nav-link link-body-emphasis text-decoration-none building-selection"
+                :class="{'bg-yellow-300': building.canonical === activeBuilding.canonical}">
+                    {{building.canonical}}
+                </div>
+            </li>
+        </ul>
+        <hr>
+        <div class="input-group">
+            <input id="new-building" type="text" class="form-control" placeholder="new-building" aria-label="New Building" aria-describedby="button-addon2"
+            v-model="newBuildingID">
+            <button 
+            :disabled="!newBuildingIDValid" 
+            class="btn"
+            :class="{'btn-primary': newBuildingIDValid, 'btn-outline-secondary': !newBuildingIDValid}" 
+            type="button" 
+            title="new ID must be at least 5 characters">
+                +
+            </button>
+            
+        </div>
+        <div class="form-text">Must be at least 5 letters</div>
+    </div>
+    </template>
+    
+    <script>
+    import {createClient} from '@supabase/supabase-js';
+    
+    
+    export default {
+        data() {
+            return {
+                newBuildingID: '',
+                activeBuilding: {},
+                buildings: [
+                    {
+                        display_name: 'Arts Building',
+                        canonical: 'arts-building',
+                    },
+                    {
+                        display_name: 'Fitzgerald Building',
+                        canonical: 'fitzgerald-building',
+                    },
+                    {
+                        display_name: 'The Pav',
+                        canonical: 'the-pav',
+                    },
+                ]
+            }
+        },
+        created() {
+            this.getListOfBuildings()
+        },
+        computed: {
+            newBuildingIDValid() {
+                return this.newBuildingID.length > 4
+            }
+        },
+        methods: {
+            async NewBuilding() {
+                console.log("Creating new building")
+                // Attempt to create a new building with the buildingID from the createNewBuilding field
+                // If successful, open the new building for editing
+                // If unsuccessful, display an error message
+            },
+    
+            async getListOfBuildings() {
+                
+                const supabaseUrl = useRuntimeConfig().public.supabaseUrl;
+                const supabaseKey = useRuntimeConfig().public.supabaseKey;
+                this.supabase = createClient(supabaseUrl, supabaseKey)
+    
+                // Select All buildings from supabase
+                // Assign them the buildings array
+
+                let { data: buildings, error } = await this.supabase
+                    .from('buildings')
+                    .select('*')
+                if (error) {
+                    console.log(error)
+                    throw error
+                }
+                else {
+                    this.buildings = buildings
+                    this.activeBuilding = buildings[0]
+                }
+            }
+        }
+    };
+    
+    useHead({
+        title: 'Acsense Admin',
+        meta: [
+            {
+                name: 'description',
+                content: 'Acsense Admin page'
+            }
+        ],
+        // link: [
+        //     {
+        //         rel: 'stylesheet',
+        //         href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css'
+        //     }
+        // ]
+    })
+    
+    </script>
+    
+    
+    <style>
+    .building-selection {
+        cursor: pointer;
+    } 
+    .building-selection:hover {
+        background-color: #e9ecef;
+    }
+    .active-building-selection {
+        background-color: #6bb5ff;
+    }
+    </style>
