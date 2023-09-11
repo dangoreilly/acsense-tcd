456e87ab1ba0d4d172fc7d95017b220f1e6242de
dangoreilly <92057656+dangoreilly@users.noreply.github.com>
2023-09-11 09:19:31 +0000

    updated modal styling

--- acsense-tcd/public/javascript/modalFunctions.js
+++ acsense-tcd/public/javascript/modalFunctions.js
@@ -43,18 +43,53 @@ function openAreaModal(area){
     let modalFoot = areaModal.querySelector('.modal-footer');
 
 
-    let capacity = `<span class="badge rounded-pill text-bg-warning" title="Capacity"><i class="fa-solid fa-chair"></i> ${area.capacity > 0 ? area.capacity : "?"}</span>`;
-    let electrical_outlets =  `<span class="badge rounded-pill text-bg-${area.electrical_outlets ? "success" : "danger"}" title="${area.electrical_outlets ? "" : "No "}Plug Sockets"><i class="fa-solid fa-plug"></i></span>`;
-    let food_drink_allowed = `<span class="badge rounded-pill text-bg-${area.food_drink_allowed ? "success" : "danger"}" title="Food and Drink ${area.food_drink_allowed ? "Allowed" : "not Allowed"}"><i class="fa-solid fa-utensils"></i></i></span>`;
-    let indoors = `<span class="badge rounded-pill text-bg-warning">${area.indoors ? "Indoors" : "Outdoors"}</span>`;
-    let wheelchair_accessible = `<span class="badge rounded-pill text-bg-${area.wheelchair_accessible ? "success" : "danger"}" title="${area.wheelchair_accessible ? "" : "No "}Wheelchair Access"><i class="fa-solid fa-wheelchair"></i></span>`;
-    let kettle = `<span class="badge rounded-pill text-bg-${area.kettle ? "success" : "danger"}" title="${area.kettle ? "" : "No "}Kettle"><i class="fa-solid fa-mug-saucer"></i></span>`;
-    let microwave = `<span class="badge rounded-pill text-bg-${area.microwave ? "success" : "danger"}" title="${area.microwave ? "" : "No "}Microwave"><i class="fa-solid fa-radiation"></i></span>`;
+    let seating = `<span 
+    class="badge rounded-pill text-bg-${area.seating ? "success" : "danger"}" 
+    title="${area.seating ? "" : "No "}Seating Available">
+    <img class="svg-icon" src="/images/icons/chair-solid.svg" width="12" height="12">
+    </span>`;
 
-    modalTitle.textContent = area.Name;
-    modalBody.textContent = area.Description;
+    let electrical_outlets = `<span 
+    class="badge rounded-pill text-bg-${area.electrical_outlets ? "success" : "danger"}" 
+    title="${area.electrical_outlets ? "" : "No "}Plug Sockets">
+    <img class="svg-icon" src="/images/icons/plug-solid.svg" width="12" height="12">
+    </span>`;
+
+    let food_drink_allowed = `<span 
+    class="badge rounded-pill text-bg-${area.food_drink_allowed ? "success" : "danger"}" 
+    title="Food and Drink ${area.food_drink_allowed ? "" : "Not "}Allowed">
+    <img class="svg-icon" src="/images/icons/utensils-solid.svg" width="12" height="12">
+    </span>`;
+
+    let wheelchair_accessible = `<span 
+    class="badge rounded-pill text-bg-${area.wheelchair_accessible ? "success" : "danger"}" 
+    title="${area.wheelchair_accessible ? "" : "No "}Wheelchair Access">
+    <img class="svg-icon" src="/images/icons/wheelchair-solid.svg" width="12" height="12">
+    </span>`;
+
+    let kettle = `<span 
+    class="badge rounded-pill text-bg-${area.kettle ? "success" : "danger"}" 
+    title="${area.kettle ? "" : "No "}Kettle Available">
+    <img class="svg-icon" src="/images/icons/kettle.svg" width="12" height="12">
+    </span>`;
+
+    let microwave = `<span 
+    class="badge rounded-pill text-bg-${area.microwave ? "success" : "danger"}" 
+    title="${area.microwave ? "" : "No "}Microwave Available">
+    <img class="svg-icon" src="/images/icons/microwave.svg" width="12" height="12">
+    </span>`;
+
+    // let electrical_outlets =  `<span class="badge rounded-pill text-bg-${area.electrical_outlets ? "success" : "danger"}" title="${area.electrical_outlets ? "" : "No "}Plug Sockets"><i class="fa-solid fa-plug"></i></span>`;
+    // let food_drink_allowed = `<span class="badge rounded-pill text-bg-${area.food_drink_allowed ? "success" : "danger"}" title="Food and Drink ${area.food_drink_allowed ? "Allowed" : "not Allowed"}"><i class="fa-solid fa-utensils"></i></i></span>`;
+    // let indoors = `<span class="badge rounded-pill text-bg-warning">${area.indoors ? "Indoors" : "Outdoors"}</span>`;
+    // let wheelchair_accessible = `<span class="badge rounded-pill text-bg-${area.wheelchair_accessible ? "success" : "danger"}" title="${area.wheelchair_accessible ? "" : "No "}Wheelchair Access"><i class="fa-solid fa-wheelchair"></i></span>`;
+    // let kettle = `<span class="badge rounded-pill text-bg-${area.kettle ? "success" : "danger"}" title="${area.kettle ? "" : "No "}Kettle"><i class="fa-solid fa-mug-saucer"></i></span>`;
+    // let microwave = `<span class="badge rounded-pill text-bg-${area.microwave ? "success" : "danger"}" title="${area.microwave ? "" : "No "}Microwave"><i class="fa-solid fa-radiation"></i></span>`;
+
+    modalTitle.textContent = area.name;
+    modalBody.textContent = area.description;
     // Combine all the information pills into one string
-    modalFoot.innerHTML = `<span class="fs-4">${indoors} ${capacity} ${electrical_outlets} ${food_drink_allowed} ${kettle} ${microwave} ${wheelchair_accessible}</span>`;
+    modalFoot.innerHTML = `<span class="fs-4">${seating} ${electrical_outlets} ${food_drink_allowed} ${kettle} ${microwave} ${wheelchair_accessible}</span>`;
 
     areaModal.style.display = "block";
 
